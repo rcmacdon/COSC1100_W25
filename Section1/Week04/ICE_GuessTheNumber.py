674a36723cea6a401c4dbcb572f6651d1546c8b8
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-02-04 09:05:11 -0500

    Section 1 Week 5

---
+++ Section1/Week04/ICE_GuessTheNumber.py
@@ -0,0 +1,83 @@
+# -----------------------------------------
+# Title: Guess the Number Game (ICE 3)
+# Author: Clint MacDonald
+# Date: Jan 31, 2025
+# Purpose: Completing ICE 3 and learning selection and iteration!
+# -----------------------------------------
+import random
+
+play = True
+maxNumber = 100
+
+# loop for each time you start over and choose the difficulty
+while play:
+    print("")
+    print("""-------------------------------------
+          I'm thinking of a number between 1 and 10, 100, or 1000...
+          Choose your difficulty level:
+          1. Easy (1-10)
+          2. Medium (1-100)
+          3. Hard (1-1000)
+          q to quit""")
+    difficulty = input("Enter your choice: ")
+
+    # determine the choice made
+    if difficulty == "1":
+        maxNumber = 10
+    elif difficulty == "2":
+        maxNumber = 100
+    elif difficulty == "3":
+        maxNumber = 1000
+    elif difficulty == "q":
+        play = False
+        continue
+    else:
+        print("Invalid choice. Please try again.")
+        continue
+
+    # 
+    playAgain = True
+    while playAgain:
+        numGuesses = 0
+        gameNumber = random.randint(1, maxNumber)
+
+        print("TEST: ", str(gameNumber))
+
+        print("I thought of a number between 1 and", maxNumber)
+        print("You have to guess it!")
+
+        guessAgain = True
+        while guessAgain:
+            guess = input("Enter your guess: ")
+
+            # check if valid
+            if not guess.isdigit():
+                print("Invalid guess. Please try again.")
+                continue
+
+            guess = int(guess)
+
+            if not 1 <= guess <= maxNumber:
+                print("Invalid guess. Please try again.")
+                continue
+
+            numGuesses += 1
+
+            if guess == gameNumber:
+                print("Congratulations! You got it right!")
+                print("You guessed the number in", numGuesses, "attempts.")
+
+                if input("Do you want to play again? (y/n):").lower() != "y":
+                    playAgain = False
+
+                guessAgain = False
+                continue
+
+            elif guess > gameNumber:
+                print("Your guess is too high, guess again!")
+            else:
+                print("Your guess is too low, guess again!")
+
+
+print("Goodbye")
+exit()
---
+++ Section1/Week05/00_DataTypes.py
@@ -0,0 +1,145 @@
+# # --------------------------------
+# # Title: Extended Data Types
+# # Author: Clint MacDonald
+# # Date: Feb. 4, 2025
+# # Purpose: To demonstrate the use of data type tools in Python
+# # --------------------------------
+
+# # play with data types
+# myInt = 5
+# myFloat = 5.0
+# myString = "hello"
+
+# # print the data types
+
+# print(type(myInt))
+# print( (type(myInt) == int) )
+
+# print(type(myFloat))
+# print(type(myString))
+
+# # --------------------------------
+# # Type Checking
+# if (type(myInt) == int):
+#     print("myInt is an int")
+
+# if (type(myFloat) == float):
+#     print("myFloat is a float")
+
+# if (myInt == myFloat):
+#     print("myInt and myFloat are equal")
+
+# # python only
+# if (isinstance(myInt, int)):
+#     print("myInt is an int")
+
+# # --------------------------------
+
+# myNumber = "5"   # string
+# print(type(myNumber))
+
+# # check if the string can be converted to an int?
+# if (myNumber.isdigit()):
+#     myNumber = int(myNumber)
+
+# print(type(myNumber))
+
+
+# # --------------------------------
+# # Try Catch Finally structure
+
+# # in Python - Try Except Finally
+
+# try:
+#     userInt = int(input("Enter a number: "))
+#     print("Conversion Successful")
+# except Exception as e:
+#     print("Conversion Failed")
+#     print(e)
+
+# print("-"*60)
+
+
+# # --------------------------------
+# # add the finally
+# try:
+#     userInt = int(input("Enter a number: "))
+#     print("Conversion Successful")  
+# except Exception as e:
+#     print("Conversion Failed")
+#     print(e)
+# finally:
+#     print("This will always run")
+
+# print("-"*60)
+
+
+# # --------------------------------
+# # pseudocode of an example of finally
+# # try:
+#     # open file
+#     # read file
+# # except:
+#     # print error message
+# # finally:
+#     # close file
+
+
+# # --------------------------------
+# # try catch with multiple exceptions
+# try:
+#     userInt = int(input("Enter a number: "))
+#     print("Conversion Successful")
+#     print(10 / userInt)
+# except ValueError as v:  # specific exception
+#     print("Conversion Failed")
+#     print("You did not enter a number")
+#     print(v)
+# except ZeroDivisionError as z:  # specific exception
+#     print("Conversion Failed")
+#     print("You cannot divide by zero")
+#     print(z)
+# except Exception as e:  # catch all
+#     print("General Error")
+#     print(e)
+# finally:
+#     print("This will always run")
+
+# # --------------------------------
+# # Nested Try Catch
+# try:
+#     userInt = int(input("Enter a number: "))
+#     print("Conversion Successful")
+    
+#     try:
+#         print(10 / userInt)
+#         print("Division Successful")
+#     except ZeroDivisionError as z:  # specific exception
+#         print("Division Failed")
+#         print("You cannot divide by zero")
+#         print(z)
+#     except Exception as e:  # catch all
+#         print("General Error")
+#         print(e)
+# except ValueError as v:
+#     print("Conversion Failed")
+#     print("You did not enter a number")
+#     print(v)
+# finally:
+#     print("This will always run")
+
+
+# --------------------------------
+# throwing exceptions
+x = 17
+y = 15
+
+try:
+    if (x > y):
+        raise Exception("x cannot be greater than y")
+except Exception as e:
+    print("An error occurred")
+    print(e)
+finally:
+    print("This will always run")
+
