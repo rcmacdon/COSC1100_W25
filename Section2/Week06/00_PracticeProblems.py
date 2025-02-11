9b21d44939f158699419c65924c3c528fed10d5f
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-02-11 10:54:00 -0500

    Added Week 6 Practice Problems for Section 2

---
+++ Section2/Week06/00_PracticeProblems.py
@@ -0,0 +1,26 @@
+# --------------------------------
+# Practice Exercises in prep for midterm
+# --------------------------------
+
+# Practice Problem 1
+# Write a program that retrieves 2 numbers from the user and prints if the first number is divisible by the second number.
+# Make sure the user can try a second time if either their input is invalid or they complete it successfully.
+
+
+
+
+# Practice Problem 2
+# Write a loop that prints even numbers between 50 and 10 backwards
+    # use a for loop
+    # use a while loop
+
+
+# Practice Problem 3
+# Write an if structure that prints if a number is positive, negative or zero
+
+
+
+# Practice Problem 4
+# Write a program that prints the sum of all numbers between 1 and 100 that are divisible by 3
+# Hint: The answer is: 1683
+
