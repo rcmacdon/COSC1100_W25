bdf17ad917828669237dfd440c45f9c54468759f
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-02-11 12:07:20 -0500

    Updated Section 2 with Desk Checks

--- Section2/Week06/00_PracticeProblems.py
+++ Section2/Week06/00_PracticeProblems.py
@@ -3,8 +3,32 @@
 # --------------------------------
 
 # Practice Problem 1
-# Write a program that retrieves 2 numbers from the user and prints if the first number is divisible by the second number.
-# Make sure the user can try a second time if either their input is invalid or they complete it successfully.
+# Write a program that retrieves 2 numbers from the user and prints if the 
+# first number is divisible by the second number.
+# Make sure the user can try a second time if either their input is invalid 
+# or they complete it successfully.
+
+# write code here!!
+
+# --------------------------------
+# Desk Checks
+# Test 1 - 10, 2    Expected Output: The numerator is divisible by the denominator
+#   Check: PASS/FAIL
+# Test 2 - 10, 3    Expected Output: The numerator is not divisible by the denominator
+#   Check: PASS/FAIL
+# Test 3 - 10, 0    Expected Output: Cannot divide by zero
+#   Check: PASS/FAIL
+# Test 4 - 3.9, 1.3 Expected Output: The numerator is divisible by the denominator
+#   Check: PASS/FAIL
+# Test 5 - 3.9, 1.2 Expected Output: The numerator is not divisible by the denominator
+#   Check: PASS/FAIL
+# Test 6 - 6, -2 Expected Output: The numerator is divisible by the denominator
+#   Check: PASS/FAIL
+# Test 7 - -6, -2    Expected Output: The numerator is divisible by the denominator
+#   Check: PASS/FAIL
+# Test 8 - 'Clint', 'dog' Expected Output: Invalid input
+#   Check: PASS/FAIL
+
 
 
 
@@ -14,13 +38,11 @@
     # use a for loop
     # use a while loop
 
-
 # Practice Problem 3
 # Write an if structure that prints if a number is positive, negative or zero
 
-
-
 # Practice Problem 4
-# Write a program that prints the sum of all numbers between 1 and 100 that are divisible by 3
+# Write a program that prints the sum of all numbers between 1 and 100 that 
+# are divisible by 3
 # Hint: The answer is: 1683
 
