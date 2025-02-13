4fe8b3d39654bd4f8eebe986cd11772bdd172c40
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-02-13 08:21:37 -0500

    Added Practice Problems for Section 1 - Thursday

---
+++ Section1/Week06/01_PracticeProblems2.py
@@ -0,0 +1,17 @@
+# ------------------------------------
+# Practice Problems 2 
+# Feb. 13, 2025
+# ------------------------------------
+# 1. Write a loop that adds up the numbers between a start number and an end number.  The numbers can be constants or input from the user.
+
+
+
+# 2. Write a program that accepts two separate inputs from the user.  One being a road speed limit and one being the speed of a car.  If the car is speeding, print out a message to the user that they are speeding.  If they are not speeding, print out a message that they are not speeding.  If they are speeding, also print out the amount they are speeding by.  
+# - The fine will be $10 per km over the limit
+# - if they are 15 over the limit they will lose 3 demerit points from their license.  
+# - if they are 30 over the limit they will lose 4 demerit points from their license.
+# - if they are 45 over the limit they will lose 5 demerit points from their license.
+# - if they are 50 over the limit they will lose 6 demerit points from their license, have their license suspended for 30 days and have their car impounded for 7 days.
+
+
+# 3. Write a program that calculates the factorial of a number.  The factorial of a number is the product of all the numbers from 1 to that number.  For example, the factorial of 5 is 5*4*3*2*1 = 120.  You do NOT need to use recursion for this problem.
