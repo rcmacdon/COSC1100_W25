2706a2ad1df6ab0e2da13d16a28fa8516b4f1e2a
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-03-06 08:12:27 -0500

    Added Pre-Practice Exercise for Week 8

---
+++ Section2/Week08 - Arrays/02_FourPlayerGolf.py
@@ -0,0 +1,25 @@
+# -------------------------------------------------
+# Title:    Week 8 - Thursday Practice Exercise
+# Date:     March 6, 2025
+# Author:
+# Purpose:  To practice using arrays
+# -------------------------------------------------
+
+# ----------------
+# Instructions
+'''
+Recreate Tuesdays Golf score application with the following changes (remember the source code is available on GitHub):
+1) There are 2 players
+2) Scores will be entered by the user for each hole.
+3) The game is a skins game, meaning that:
+- the player that gets the best score on a hole wins the hole and gets 1 point.  
+- If 2 players tie, the hole is carried over and the player who individually wins the next hole gets 2 points, (or 3 points if the previous 2 holes were held over) 
+- The overall winner will be the player whom gets the most points.
+4) At the end, output a list of holes, the score each player got, par, and the type of score the player got according to the following data:
+    -3 - albatross
+    -2 - eagle
+    -1 - birdie
+     0 - par
+    +1 - bogie
+    +2 - double bogie
+''' # ----------------
