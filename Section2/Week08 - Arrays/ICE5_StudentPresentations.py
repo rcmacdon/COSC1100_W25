6bcba6fd986af25070c6fa09a644f8ba73ad513e
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-03-07 14:05:14 -0500

    Added ICE5 Starter Code for Section 2

---
+++ Section2/Week08 - Arrays/ICE5_StudentPresentations.py
@@ -0,0 +1,30 @@
+# ------------------------------------------
+# Title: ICE5 - Student Presentations (Arrays and Lists)
+# Author:
+# Date:
+# Purpose: To demonstrate your knowledge of arrays and lists in Python.
+# ------------------------------------------
+
+#region Declarations
+studentList = []
+ALLOW_DUPLICATES = False
+menuText = """
+------------------------------------
+Main Menu
+1. Add Student to the Queue
+2. View Entire Student Queue
+3. Call Next Student in Queue
+4. Remove Student From Queue
+5. Quit"""
+#endregion
+
+#region Main Application
+# NOTE: replace <YOURNAME> with your name below!
+print('-'*40)
+print("Welcome to <YOURNAME>'s Student Queue Application!")
+print('-'*40)
+
+
+
+
+#endregion
