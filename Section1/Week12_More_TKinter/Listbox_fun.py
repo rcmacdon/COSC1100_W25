7fd1e2cf153cdfbbb700d6f73a9ed9bdf98b73e4
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-04-03 10:12:05 -0400

    start of week 12 demo

---
+++ Section1/Week12_More_TKinter/Listbox_fun.py
@@ -0,0 +1,32 @@
+# -------------------------------
+# Title:    Listbox - one-to-many relationship choices
+# Author:   Clint MacDonald
+# Date:     April 3, 2025
+# Purpose:  To work more with TKinter and some more interface choices
+# -------------------------------
+
+#region IMPORTS
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from idlelib.tooltip import Hovertip
+#endregion
+
+#region GLOBAL VARIABLES
+#endregion
+
+#region FUNCTIONS
+
+#region tools
+#endregion
+
+#region events
+#endregion
+
+#endregion
+
+#region interface
+#endregion
+
+#region main
+#endregion
